05a2e7af94d61f4ed982c1fa6129b04bfd6c9b4f
Darealtube <evajavier143@gmail.com>
2020-08-16 14:54:41 +0800

    Patch 1.0

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Pomodoro />
   </React.StrictMode>,
   document.getElementById('root')
 );
